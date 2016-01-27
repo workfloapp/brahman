0e6eb660b4aaad44ba960bfc6e6a4c60d50d7e3b
Jannis Pohlmann <jannis@xfce.org>
2016-01-27 02:37:45 +0100

    brahman.test.setup: Add helpers for using Datomic fixtures in tests

---
+++ test/brahman/test/setup.clj
@@ -0,0 +1,21 @@
+(ns brahman.test.setup
+  (:import [datomic.peer Connection])
+  (:require [clojure.test :refer [deftest is]]
+            [datomic.api :as d]))
+
+(def db-uri "datomic:free://localhost:4334/brahman-test")
+
+(defn create-db []
+  (d/create-database db-uri))
+
+(defn delete-db []
+  (d/delete-database db-uri))
+
+(defn connect-to-db []
+  (d/connect db-uri))
+
+(defmacro with-conn
+  [& body]
+  `(let [~(symbol "conn") (connect-to-db)]
+     (do
+       ~@body)))
