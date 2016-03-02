af37069674215e0b42f1a4e04e9a6e76f2cf19f5
Jannis Pohlmann <contact@jannispohlmann.de>
2016-03-02 11:58:22 +0100

    Move schema attributes extraction into brahman.backends.datascript
    Also alias it in brahman.backends.datomic for backwards-compatibility.

--- src/brahman/backends/datascript.cljc
+++ src/brahman/backends/datascript.cljc
@@ -1,6 +1,51 @@
 (ns brahman.backends.datascript
   (:require [brahman.model :as bm]))
 
+;;;; Schema attribute extraction
+
+(defn- attr-name [schema-name short-name]
+  (keyword (name schema-name) (name short-name)))
+
+(defn- join? [[short-name spec]]
+  (and (map? spec) (= 1 (count spec))))
+
+(defn- gather-attr-names [model-name schema]
+  {:pre [(map? schema)]}
+  (into []
+        (comp (remove join?)
+              (map (fn [[name attr-spec]]
+                     (attr-name model-name name))))
+        schema))
+
+(declare schema->attrs)
+
+(defn- gather-joins [model schema]
+  {:pre [(map? schema)]}
+  (letfn [(recur-join [target-name]
+            (let [modeler (bm/get-modeler model)
+                  target  (bm/get-model modeler target-name)]
+              (schema->attrs target)))
+          (join-attr [schema-name short-name spec]
+            (let [[spec target] (first spec)]
+              (hash-map (attr-name schema-name short-name)
+                        (cond-> target
+                          (not= target '...) recur-join))))
+          (gather-join [res [short-name spec]]
+            (cond-> res
+              (map? spec) (conj (join-attr (bm/model-name model)
+                                           short-name
+                                           spec))))]
+    (reduce gather-join [] schema)))
+
+(defn schema->attrs
+  [model]
+  (let [name       (bm/model-name model)
+        schema     (bm/schema model)
+        attr-names (gather-attr-names name schema)
+        joins      (gather-joins model schema)
+        ret        (into [] (concat [:db/id] attr-names joins))]
+    ret))
+
 ;;;; Schema generation
 
 (defn datascript-schema [[model attrs]]
--- src/brahman/backends/datomic.clj
+++ src/brahman/backends/datomic.clj
@@ -1,53 +1,12 @@
 (ns brahman.backends.datomic
   (:require [datomic.api :as d]
             [datomic-schema.schema :as s]
+            [brahman.backends.datascript :as bds]
             [brahman.model :as bm]))
 
 ;;;; Schema attribute extraction
 
-(defn- attr-name [schema-name short-name]
-  (keyword (name schema-name) (name short-name)))
-
-(defn- join? [[short-name spec]]
-  (and (map? spec) (= 1 (count spec))))
-
-(defn- gather-attr-names [model-name schema]
-  {:pre [(map? schema)
-         (contains? schema :attrs)
-         (map? (:attrs schema))]}
-  (into []
-        (comp (remove join?)
-              (map (fn [[name attr-spec]]
-                     (attr-name model-name name))))
-        (:attrs schema)))
-
-(declare schema->attrs)
-
-(defn- gather-joins [model-name schema]
-  {:pre [(map? schema)
-         (contains? schema :name)
-         (contains? schema :attrs)
-         (map? (:attrs schema))]}
-  (letfn [(join-attr [schema-name short-name spec]
-            (let [[spec target] (first spec)]
-              (hash-map (attr-name schema-name short-name)
-                        (cond-> target
-                          (not= target '...) schema->attrs))))
-          (gather-join [res [short-name spec]]
-            (cond-> res
-              (map? spec) (conj (join-attr model-name
-                                           short-name
-                                           spec))))]
-    (reduce gather-join [] (:attrs schema))))
-
-(defn schema->attrs
-  [model]
-  (let [name       (bm/model-name model)
-        schema     (bm/schema model)
-        attr-names (gather-attr-names name schema)
-        joins      (gather-joins name schema)
-        ret (into [] (concat [:db/id] attr-names joins))]
-    ret))
+(def schema->attrs bds/schema->attrs)
 
 ;;;; Schema generation
 
